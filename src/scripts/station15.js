5b3b8b8800abf31d6ab7f9bd7bc6e82cf3754d89
ojiro-doi <sakura.0046@icloud.com>
2024-04-10 09:26:34 +0900

    クリア

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -2,15 +2,35 @@ async function getData() {
   // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
+
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList);
+  return await result;
 }
 
-function test() {
-  return
+function test(userList) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const results = userList.map(data => {
+        const full_name = `${data.family_name} ${data.first_name}`;
+        return { ...data, full_name };
+      });
+      resolve(results);
+    }, 3000);
+  });
 }
-
